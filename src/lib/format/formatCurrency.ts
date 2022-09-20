21eccffc9149c63b63012c78b6b9e395386e096f
kaen7 <kusdsuna@naver.com>
2022-09-20 10:05:05 +0900

    chore: chartRate format digit flag update
    tooltip formatCurrency digit update

--- src/lib/format/formatCurrency.ts
+++ src/lib/format/formatCurrency.ts
@@ -1,12 +1,19 @@
-import {utils} from "ethers";
+import { utils } from "ethers";
 export const formatCurrency = (value, decimals = 2) => {
 	if (!value) return "0";
 
+	// todo 소수점 둘째자리까지 0이 아니면 소수점 처리
+
 	const cutValue = 10n ** BigInt(18 - decimals);
 	const cutDecimals = (value / cutValue) * cutValue;
-	const addComma = Number(utils.formatEther(cutDecimals)).toLocaleString("en", {
-		maximumFractionDigits: decimals,
-	});
+	const addComma =
+		utils.formatEther(cutDecimals).split(".")[1][2] === "0"
+			? Number(utils.formatEther(cutDecimals)).toLocaleString("en", {
+					maximumFractionDigits: decimals,
+			  })
+			: Number(utils.formatEther(cutDecimals)).toLocaleString("en", {
+					maximumFractionDigits: 2,
+			  });
 
 	return addComma ? addComma : cutDecimals.toLocaleString();
 };
